ab45a3cc190757d5fbb4a2111c5f452aedc69610
Yashvi Chauhan <yashvi.chauhan1909@gmail.com>
2026-02-15 22:35:10 +0530

    day11

---
+++ day12.c
@@ -0,0 +1,37 @@
+//symmetric matrix
+#include <stdio.h>
+
+int main() {
+    int m, n;
+
+    scanf("%d %d", &m, &n);
+
+    int a[100][100];
+
+    // Input matrix
+    for(int i = 0; i < m; i++) {
+        for(int j = 0; j < n; j++) {
+            scanf("%d", &a[i][j]);
+        }
+    }
+
+    // Check if square
+    if(m != n) {
+        printf("Not a Symmetric Matrix");
+        return 0;
+    }
+
+    // Check symmetry
+    for(int i = 0; i < m; i++) {
+        for(int j = 0; j < n; j++) {
+            if(a[i][j] != a[j][i]) {
+                printf("Not a Symmetric Matrix");
+                return 0;
+            }
+        }
+    }
+
+    printf("Symmetric Matrix");
+
+    return 0;
+}
